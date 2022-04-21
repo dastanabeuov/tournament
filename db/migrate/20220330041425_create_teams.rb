29e897c5e348a6d3ba0438cfa9c548c44285ce4f
ADK <web.dev.adk@gmail.com>
2022-04-21 10:22:54 +0600

    remove field team best

--- db/migrate/20220330041425_create_teams.rb
+++ db/migrate/20220330041425_create_teams.rb
@@ -2,7 +2,6 @@ class CreateTeams < ActiveRecord::Migration[7.0]
   def change
     create_table :teams do |t|
       t.string :name, null: false
-      t.boolean :best, default: false, null: false
       t.integer :tournament_id, null: false, foreign_key: true
       t.integer :user_id, null: false, foreign_key: true
 
