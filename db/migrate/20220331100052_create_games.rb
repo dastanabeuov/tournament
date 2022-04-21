dbd239db0165a1a5d68207f189aed3e8521fee6c
ADK <web.dev.adk@gmail.com>
2022-04-21 10:42:33 +0600

    add winner fileds from game table

--- db/migrate/20220331100052_create_games.rb
+++ db/migrate/20220331100052_create_games.rb
@@ -1,14 +1,15 @@
 class CreateGames < ActiveRecord::Migration[7.0]
   def change
     create_table :games do |t|
-      t.integer :user_id, null: false, foreign_key: true
-      t.integer :tournament_id, null: false, foreign_key: true
-      t.integer :team_one_id, null: false, foreign_key: true
-      t.integer :team_two_id, null: false, foreign_key: true
+      t.integer  :user_id, null: false, foreign_key: true
+      t.integer  :tournament_id, null: false, foreign_key: true
+      t.integer  :team_one_id, null: false, foreign_key: true
+      t.integer  :team_two_id, null: false, foreign_key: true
       t.datetime :datetime
-      t.integer :tour
-      t.integer :goals_one
-      t.integer :goals_two
+      t.integer  :tour
+      t.string   :winner
+      t.integer  :goals_one
+      t.integer  :goals_two
 
       t.timestamps
     end
